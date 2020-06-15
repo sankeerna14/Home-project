99ea46c2b9c7f5f9f14a427579477cefa8aaca45
Sankeerna <sankeerna.reddy14@gmail.com>
2020-06-15 16:16:34 -0700

    line spacing

--- app/adapters/application.js
+++ app/adapters/application.js
@@ -1,4 +1,5 @@
 import JSONAPIAdapter from '@ember-data/adapter/json-api';
+
 export default class ApplicationAdapter extends JSONAPIAdapter {
     host = 'https://johnny-appleseed.clientsecure.me';
     namespace = 'client-portal-api';
